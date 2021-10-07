8e7988f10d7b4bdc4a60e6fb132050873b89ccaa
Georgios Andrianakis <geoand@gmail.com>
2021-10-07 11:10:00 +0300

    Use more Java 17 goodness in mail-service

--- exercise-service/src/main/java/io/quarkus/demo/exercise/ExerciseResource.java
+++ exercise-service/src/main/java/io/quarkus/demo/exercise/ExerciseResource.java
@@ -3,30 +3,37 @@ package io.quarkus.demo.exercise;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
 
 @Path("exercise")
 public class ExerciseResource {
 
-    @Path("workout/random")
+    public static final RandomGenerator RANDOM_GENERATOR = RandomGeneratorFactory.getDefault().create();
+
     @GET
+    @Path("workout/random")
     public List<ExerciseDTO> randomWorkout() {
-        Optional<ExerciseDTO> cardio = randomExercise(Exercise.list("type", ExerciseType.Cardio.name()));
-        Optional<ExerciseDTO> weights = randomExercise(Exercise.list("type", ExerciseType.Weights.name()));
+        List<Exercise> allWeights = Exercise.list("type", ExerciseType.Weights.name());
+        List<Exercise> allCardio = Exercise.list("type", ExerciseType.Cardio.name());
+        var cardio = random(allCardio);
+        var weights = random(allWeights);
         if (cardio.isEmpty() || weights.isEmpty()) {
-            throw new WebApplicationException("Unable to obtain random exercise", Response.Status.INTERNAL_SERVER_ERROR);
+            throw new WebApplicationException("Unable to obtain a random workout");
         }
-        return List.of(cardio.get(), weights.get());
+        return List.of(fromExercise(cardio.get()), fromExercise(weights.get()));
     }
 
-    private Optional<ExerciseDTO> randomExercise(List<Exercise> all) {
+    private Optional<Exercise> random(List<Exercise> all) {
         if (all.isEmpty()) {
             return Optional.empty();
         }
-        Exercise exercise = all.get(ThreadLocalRandom.current().nextInt(all.size()));
-        return Optional.of(new ExerciseDTO(exercise.name, ExerciseType.valueOf(exercise.type)));
+        return Optional.of(all.get(RANDOM_GENERATOR.nextInt(all.size())));
+    }
+
+    private ExerciseDTO fromExercise(Exercise exercise) {
+        return new ExerciseDTO(exercise.name, ExerciseType.valueOf(exercise.type));
     }
 }
